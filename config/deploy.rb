bec671fb280f0e350bd846df2eae42e963de6ecc
samhecquet <samhecquet@gmail.com>
2016-12-02 18:15:00 +0000

    try to fix capistrano asking wrong user

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,11 @@
 # config valid only for current version of Capistrano
 lock '3.6.1'
 # Change these
-server '159.203.32.234', port: 22, roles: [:web, :app, :db], primary: true
+server '159.203.32.234', user: fetch(:user), port: 22, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:SamHecquet/the-missing-music-api.git'
 set :application,     'the-missing-music-api'
 set :user,            'deploy'
-set :deploy_user,     'deploy'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
@@ -14,7 +13,7 @@ set :puma_workers,    0
 set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
-set :deploy_via,      :copy
+set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
@@ -22,7 +21,6 @@ set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
 # set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
-set :ssh_options, {:forward_agent => true}
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
